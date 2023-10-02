6b0abe1793d1b920847d8e1fe2e1b9a0f907c3e0
Bhanu pratap Behera <bhanupratapbehera193@gmail.com>
2023-10-02 21:00:57 +0530

    issue of deployment fixed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,8 @@ import orderRouter from "./router/order";
 import { dbConnect } from "./configs/database.config";
 dbConnect();
 const app = express();
+
+
 app.use(express.json());
 app.use(
   cors({
@@ -18,12 +20,11 @@ app.use(
     origin: ["http://localhost:4200"],
   })
 );
-// app.get("/", (req, res) => {
-//   res.send("Welcome To Foodmine API");
-// });
+
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+app.use(express.static("public"));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
