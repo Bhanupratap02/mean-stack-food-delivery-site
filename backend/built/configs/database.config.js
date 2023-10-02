1c25e92900126e4dcb4d891816ca130563891278
Bhanu pratap Behera <bhanupratapbehera193@gmail.com>
2023-10-02 20:37:53 +0530

    ready to deploy

--- backend/built/configs/database.config.js
+++ backend/built/configs/database.config.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConnect = void 0;
 var mongoose_1 = require("mongoose");
 var dbConnect = function () {
-    (0, mongoose_1.connect)(process.env.MONGO_URI, {
+    (0, mongoose_1.connect)("mongodb+srv://bhanupratapdev02:Sachin0211@cluster0.bmv8ln5.mongodb.net/foodmine?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
